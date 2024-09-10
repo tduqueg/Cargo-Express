fae7445c4883412869689fad1c43cd076de379e5
tduqueg <tduqueg@eafit.edu.co>
2024-09-09 21:13:15 -0500

    Catalogo de productos y flota de repartidores

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 from database import conn
@@ -6,23 +6,54 @@ import sqlite3
 
 app = FastAPI()
 
+# Modelo de datos para los productos
+class Producto(BaseModel):
+    id_producto: str
+    cantidad: int
+
 # Modelo de datos para los pedidos
 class Pedido(BaseModel):
-    id_producto: str
+    productos: list[Producto]  # Cambiado de id_producto a productos
     id_repartidor: str
-    cantidad: int
     fecha_entrega: datetime
 
-# Endpoit registro pedidos
+# Endpoint registro pedidos
 @app.post("/entrega")
 async def registrar_pedido(pedido: Pedido):
     cursor = conn.cursor()
 
-    fecha_entrega_str = pedido.fecha_entrega.isoformat()
+    # Verificamos la existencia del repartidor en la tabla repartidores
+    cursor.execute('SELECT COUNT(*) FROM repartidores WHERE id_repartidor = ?', (pedido.id_repartidor,))
+    if cursor.fetchone()[0] == 0:
+        print(f"Repartidor {pedido.id_repartidor} no encontrado en la base de datos")
+        raise HTTPException(status_code=400, detail="Repartidor no válido")
 
-    cursor.execute('''
-    INSERT INTO pedidos (id_producto, id_repartidor, cantidad, fecha_entrega)
-    VALUES (?, ?, ?, ?)
-        ''', (pedido.id_producto, pedido.id_repartidor, pedido.cantidad, fecha_entrega_str))
+    # Verificamos la existencia de cada producto en la tabla productos
+    for producto in pedido.productos:
+        cursor.execute('SELECT COUNT(*) FROM productos WHERE id_producto = ?', (producto.id_producto,))
+        if cursor.fetchone()[0] == 0:
+            raise HTTPException(status_code=400, detail=f"Producto {producto.id_producto} no válido")
+
+        # Insertamos cada producto en la base de datos
+        fecha_entrega_str = pedido.fecha_entrega.isoformat()
+        cursor.execute('''
+        INSERT INTO pedidos (id_producto, id_repartidor, cantidad, fecha_entrega)
+        VALUES (?, ?, ?, ?)
+        ''', (producto.id_producto, pedido.id_repartidor, producto.cantidad, fecha_entrega_str))
+
     conn.commit()
-    return {"mensaje": "Pedido registrado exitosamente", "data": pedido}
+    return {"mensaje": "Pedidos registrados exitosamente", "data": pedido}
+
+@app.get("/repartidores")
+async def obtener_repartidores():
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM repartidores')
+    rows = cursor.fetchall()
+    return {"repartidores": rows}
+
+@app.get("/productos")
+async def obtener_repartidores():
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM productos')
+    rows = cursor.fetchall()
+    return {"productos": rows}
